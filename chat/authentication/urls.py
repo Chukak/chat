f8e8c44c025c734ee8839a04adc9aa4c79ffb48a
chukak <chukak12@gmail.com>
2018-03-08 21:21:40 +0300

    Update authentication urls

--- chat/authentication/urls.py
+++ chat/authentication/urls.py
@@ -1,14 +1,16 @@
 from django.conf.urls import url
-from authentication.views import RegisterView, logout_view
+from authentication.views import RegisterView, LoginView, logout_view
 
 """
 Urls configuration.
 1. register/ - registration page, url - register/
-2. logout/ - logout page, url - logout/
+2. login/ - login page, url - login/
+3. logout/ - logout page, url - logout/
 
 """
 app_name = 'authentication'
 urlpatterns = [
      url(r'register/', RegisterView.as_view(), name='register'),
+     url(r'login/', LoginView.as_view(), name='login'),
      url(r'logout/', logout_view, name='logout'),
 ]
