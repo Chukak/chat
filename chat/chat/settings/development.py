3750f346a68f22ead891b86cb99751952f9b5c1f
chukak <chukak12@gmail.com>
2018-03-08 20:36:01 +0300

    Fix development and production settings

---
+++ chat/chat/settings/development.py
@@ -0,0 +1,17 @@
+"""
+Development settings.
+Note: import only base setting. Don`t use this for something else.
+"""
+from .base import *
+
+DEBUG = True
+
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+
+# Sqlite3
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
---
+++ chat/chat/settings/production.py
@@ -0,0 +1,24 @@
+"""
+Production settings.
+Note: import only base setting. Don`t use this for something else.
+"""
+from .base import *
+
+DEBUG = False
+
+"""
+Add your valid hosts here.
+"""
+ALLOWED_HOSTS = []
+
+"""
+Production has only mysql database.
+"""
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'read_default_file': os.path.join(BASE_DIR, 'mysql.cnf'),
+        },
+    }
+}
