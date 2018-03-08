00cce70933746bf6880f9665806498dc82fb5d07
chukak <chukak12@gmail.com>
2018-03-08 21:43:33 +0300

    Update chat app urls

--- chat/chat_app/urls.py
+++ chat/chat_app/urls.py
@@ -1,5 +1,12 @@
 from django.conf.urls import url
+from .views import ChatView
 
+"""
+Urls configuration.
+1. '' - char page, url - chat/
 
-app_name = 'chat'
-urlpatterns = []
+"""
+app_name = "chat"
+urlpatterns = [
+    url(r'', ChatView.as_view(), name="chat"),
+]
