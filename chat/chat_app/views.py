8c833de44019c3fce388f0e52380ce4bb3a86b0f
chukak <chukak12@gmail.com>
2018-03-08 21:41:35 +0300

    Create chat views

--- chat/chat_app/views.py
+++ chat/chat_app/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+
+class ChatView(TemplateView):
+    """
+    Chat view.
+
+    Show homepage template.
+
+    """
+    template_name = "chat_app/chat.html"
+
+    @login_required(redirect_field_name=None, login_url="/")
+    def dispatch(self, request, *args, **kwargs):
+        """
+        Checks is user is authenticated, return `dispatch` method.
+        Otherwise returns redirect to `login_url`.
+        """
+        return super().dispatch(request, *args, **kwargs)
